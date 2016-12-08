e8bfb39fcf0a8f095714cbfbe4def518e2bcf4ce
Chris Blazer <ccblazer44@gmail.com>
2016-12-07 17:38:17 -0700

    added code from class

--- ezview.c
+++ ezview.c
@@ -27,6 +27,7 @@ Image *image;
 
 static void read_ppm(char *filename){
   FILE *fh;
+  int c;
 
   // open file
   fh = fopen(filename, "rb");
@@ -73,8 +74,8 @@ static void read_ppm(char *filename){
       fprintf(stderr, "Error reading height and width\n");
       exit(1);
   }
-  width = image->width;
-  height = image->height;
+  int width = image->width;
+  int height = image->height;
 
   // get RGB_NUMBER from image and comapre to defined RGB_NUMBER
   int rgb_num;
@@ -84,20 +85,111 @@ static void read_ppm(char *filename){
       exit(1);
   }
 
+  // allocate memory
+  image->data = (RGBpixel*)malloc((RGB_NUMBER + 1) * width * height);
+  if(!image->data){
+    fprintf(stderr, "Error allocating memory\n");
+    exit(1);
+  }
 
   fgetc(fh);
 
   //check if p3 or p6
+  if(image->magicNum == '3'){
+    int temp;
+    for (int i = 0; i < height*width; i++){
+        fscanf(fh, "%d", &temp);
+        image->data[i].r = temp;
+        fscanf(fh, "%d", &temp);
+        image->data[i].g = temp;
+        fscanf(fh, "%d", &temp);
+        image->data[i].b = temp;
+    }
+  }
+
+  if(image->magicNum == '6'){
+    fread(image->data, (sizeof(&image->data)), height * width, fh);
+  }
+
+  fclose(fh);
 
 } // end read function
 
+typedef struct {
+  float Position[2];
+  float TexCoord[2];
+} Vertex;
 
+// (-1, 1)  (1, 1)
+// (-1, -1) (1, -1)
 
+Vertex vertexes[] = {
+  {{-1, 1}, {0, 0}}, //Bottom Left
+  {{1, 1},  {1, 0}},  //Bottom Right
+  {{-1, -1},  {0, 1}},  //Top Left
 
+  {{1, 1}, {1, 0}}, //Bottom Right
+  {{1, -1},  {1, 1}}, //Top Right
+  {{-1, -1},  {0, 1}} //Top Left
+
+};
+
+const GLubyte Indices[] = {
+  0, 1, 2,
+  2, 3, 0
+};
+
+static const char* vertex_shader_text =
+"uniform mat4 MVP;\n"
+"attribute vec2 TexCoordIn;\n"
+"attribute vec2 vPos;\n"
+"varying vec2 TexCoordOut;\n"
+"void main()\n"
+"{\n"
+"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"    TexCoordOut = TexCoordIn;\n"
+"}\n";
+
+static const char* fragment_shader_text =
+"varying lowp vec2 TexCoordOut;\n"
+"uniform sampler2D Texture;\n"
+"void main()\n"
+"{\n"
+"    gl_FragColor = texture2D(Texture, TexCoordOut);\n"
+"}\n";
+
+void glCompileShaderOrDie(GLuint shader) {
+  GLint compiled;
+  glCompileShader(shader);
+  glGetShaderiv(shader,
+		GL_COMPILE_STATUS,
+		&compiled);
+  if (!compiled) {
+    GLint infoLen = 0;
+    glGetShaderiv(shader,
+		  GL_INFO_LOG_LENGTH,
+		  &infoLen);
+    char* info = malloc(infoLen+1);
+    GLint done;
+    glGetShaderInfoLog(shader, infoLen, &done, info);
+    printf("Unable to compile shader: %s\n", info);
+    exit(1);
+  }
+}
 
 
 
 
 int main(int argc, char *argv[]){
+
+  if (argc != 2){
+      printf("usage: ezview input.ppm");
+      return(1);
+  }
+
+  char* inFile = argv[1];
+  read_ppm(inFile);
+
+  return(1);
 
 }
