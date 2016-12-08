258d13e9ebe45f4639966699cf63daa164884b20
Chris Blazer <ccblazer44@gmail.com>
2016-12-08 16:51:54 -0700

    added sheer and scale.  Project done

--- ezview.c
+++ ezview.c
@@ -1,21 +1,19 @@
 #define GLFW_DLL 1
-
 #define GL_GLEXT_PROTOTYPES
+
 #include <GLES2/gl2.h>
 #include <GLFW/glfw3.h>
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include "linmath.h"
 
-#include <assert.h>
-
 #define RGB_NUMBER 255
 
+// global variables for affine transformations
 float trans_x = 0, trans_y = 0, scale = 1, rotation = 0, sheer = 0;
-
-
 
 typedef struct RGBpixel{
     unsigned char r,g,b;
@@ -123,9 +121,6 @@ typedef struct {
   float TexCoord[2];
 } Vertex;
 
-// (-1, 1)  (1, 1)
-// (-1, -1) (1, -1)
-
 Vertex vertexes[] = {
   {{-1, 1}, {0, 0}}, //Bottom Left
   {{1, 1},  {1, 0}},  //Bottom Right
@@ -180,6 +175,7 @@ void glCompileShaderOrDie(GLuint shader) {
   }
 }
 
+// key commands to use affine transformations
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
       glfwSetWindowShouldClose(window, GLFW_TRUE);
@@ -202,8 +198,18 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
     if (key == GLFW_KEY_W && action == GLFW_PRESS){
       rotation -= 0.1;
     }
-
-
+    if (key == GLFW_KEY_A && action == GLFW_PRESS){
+      scale -= 0.1;
+    }
+    if (key == GLFW_KEY_S && action == GLFW_PRESS){
+      scale += 0.1;
+    }
+    if (key == GLFW_KEY_Z && action == GLFW_PRESS){
+      sheer -= 0.1;
+    }
+    if (key == GLFW_KEY_X && action == GLFW_PRESS){
+      sheer += 0.1;
+    }
 }
 
 static void error_callback(int error, const char* description){
@@ -230,7 +236,6 @@ int main(int argc, char *argv[]){
     exit(EXIT_FAILURE);
   }
 
-
       glfwDefaultWindowHints();
       glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
       glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
@@ -246,15 +251,11 @@ int main(int argc, char *argv[]){
   glfwSetKeyCallback(window, key_callback);
 
   glfwMakeContextCurrent(window);
-  // gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
   glfwSwapInterval(1);
-
-  // NOTE: OpenGL error checks have been omitted for brevity
 
   glGenBuffers(1, &vertex_buffer);
   glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertexes), vertexes, GL_STATIC_DRAW);
-
 
   vertex_shader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
@@ -268,7 +269,6 @@ int main(int argc, char *argv[]){
   glAttachShader(program, vertex_shader);
   glAttachShader(program, fragment_shader);
   glLinkProgram(program);
-  // more error checking! glLinkProgramOrDie!
 
   mvp_location = glGetUniformLocation(program, "MVP");
   assert(mvp_location != -1);
@@ -299,9 +299,6 @@ int main(int argc, char *argv[]){
       (void*) (sizeof(float) * 2));
 
 
-  // int image_width = 4;
-  // int image_height = 4;
-
   GLuint texID;
   glGenTextures(1, &texID);
   glBindTexture(GL_TEXTURE_2D, texID);
@@ -319,7 +316,7 @@ int main(int argc, char *argv[]){
   {
       float ratio;
       int width, height;
-      mat4x4 m, p, mvp, trans, rotat;
+      mat4x4 m, p, mvp, trans, rotat, scl, shr;
 
       glfwGetFramebufferSize(window, &width, &height);
       ratio = width / (float) height;
@@ -329,19 +326,23 @@ int main(int argc, char *argv[]){
 
       mat4x4_identity(m); // main matrix
 
+      // affine transformations
       mat4x4_identity(trans); // translation matrix
-      mat4x4_translate(trans,trans_x,trans_y,0);  // translate
+      mat4x4_translate(trans, trans_x, trans_y,0);  // translate
       mat4x4_add(m,trans,m); // combine translation and main matrix
 
       mat4x4_identity(rotat); // rotation matrix
       mat4x4_rotate_Z(rotat, rotat, rotation); // rotate
-      mat4x4_mul(m,rotat,m); // combine rotation and main matrix
+      mat4x4_mul(m ,rotat, m); // combine rotation and main matrix
 
+      mat4x4_identity(shr); // sheer matrix
+      mat4x4_shear(shr, shr, sheer, sheer);  // sheer
+      mat4x4_add(m, shr, m); // combine sheer and main matrix
 
-
-
-
-
+      mat4x4_identity(scl); // scale matrix
+      mat4x4_scale_aniso(scl, scl, scale, scale, scale);  // scale
+      mat4x4_add(m, scl, m); // combine scale and main matrix
+      // end affine transformations
 
       mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
       mat4x4_mul(mvp, p, m);
